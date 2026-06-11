29e18539b4a3ba49666a0aeb17f32499c132d467
Sukhim2003 <sukhim2003@gmail.com>
2026-06-11 12:28:41 +0300

    add by Mokhtar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,52 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
+
 <head>
     <meta charset="UTF-8">
     <title>نظام إدارة تأجير السيارات</title>
     <style>
-        body{
+        body {
             font-family: Arial, sans-serif;
-            text-align:center;
-            margin-top:100px;
-            background:#f5f5f5;
+            text-align: center;
+            margin-top: 100px;
+            background: #f5f5f5;
         }
 
-        .container{
-            background:white;
-            width:600px;
-            margin:auto;
-            padding:30px;
-            border-radius:10px;
-            box-shadow:0 0 10px rgba(0,0,0,.1);
+        .container {
+            background: white;
+            width: 600px;
+            margin: auto;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, .1);
         }
 
-        h1{
-            color:#0d6efd;
+        h1 {
+            color: #0d6efd;
         }
     </style>
 </head>
+
 <body>
 
-<div class="container">
-    <h1>نظام إدارة تأجير السيارات</h1>
+    <div class="container">
+        <h1>نظام إدارة تأجير السيارات</h1>
 
-    <p>
-        مشروع تدريبي باستخدام Laravel 12
-    </p>
+        <p>
+            مشروع تدريبي باستخدام Laravel 12
+        </p>
 
-    <p>
-        تم إعداد المشروع وربطه مع GitHub بنجاح ✅
-    </p>
-    <p> 
-        اعداد المهندس سهيل
-    </p>
-</div>
+        <p>
+            تم إعداد المشروع وربطه مع GitHub بنجاح ✅
+        </p>
+        <p>
+            اعداد المهندس سهيل
+        </p>
+        <p>
+            اعداد المهندس مختار
+        </p>
+    </div>
 
 </body>
+
 </html>
